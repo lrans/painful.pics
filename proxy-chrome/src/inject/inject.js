643219fef30ed5072577fc4429f78eb197e77b0f
snarl <snarlwolfy@gmail.com>
2016-07-19 22:50:52 -0400

    Fixed proxy chrome injection for remote

--- proxy-chrome/src/inject/inject.js
+++ proxy-chrome/src/inject/inject.js
@@ -2,7 +2,7 @@ chrome.extension.sendMessage({}, function (response) {
 	var readyStateCheckInterval = setInterval(function () {
 		if (document.readyState === "complete") {
 			clearInterval(readyStateCheckInterval);
-			var scriptContent = 'proxy.availableMethods.chromeExtension = { id: "' + chrome.runtime.id + '" };';
+			var scriptContent = 'if (typeof proxy !== "undefined") { proxy.availableMethods.chromeExtension = { id: "' + chrome.runtime.id + '" }; }';
 			var script = document.createElement('script');
 			script.id = 'tmpScript';
 			script.appendChild(document.createTextNode(scriptContent));
