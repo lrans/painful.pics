9df3a07ab7e0815335e310b4b9ff7e513be84d15
snarl <snarlwolfy@gmail.com>
2016-07-24 16:49:11 -0400

    Fixed tracking of relevant browser tabs

--- proxy-chrome/src/bg/background.js
+++ proxy-chrome/src/bg/background.js
@@ -1,32 +1,49 @@
 /* global chrome */
 
 var faCookiesString;
+var painfulPicsTabIDs = [];
 
+/**
+ * Adds FA cookies to relevant requests.
+ */
 var requestListener = function (details) {
-	chrome.extension.getBackgroundPage().console.log("request listener triggered");
-	details.requestHeaders.push({
-		name: 'cookie',
-		value: faCookiesString
-	});
-	return {requestHeaders: details.requestHeaders};
+	if (painfulPicsTabIDs.indexOf(details.tabId) > -1) {
+		chrome.extension.getBackgroundPage().console.log("request listener triggered");
+		details.requestHeaders.push({
+			name: 'cookie',
+			value: faCookiesString
+		});
+		return {requestHeaders: details.requestHeaders};
+	} else {
+		return {};
+	}
 };
 
+/**
+ * Adds CORS headers to responses.
+ */
 var responseListener = function (details) {
-	chrome.extension.getBackgroundPage().console.log("response listener triggered");
-	var rule = {
-		"name": "Access-Control-Allow-Origin",
-		"value": "*"
-	};
+	if (painfulPicsTabIDs.indexOf(details.tabId) > -1) {
+		chrome.extension.getBackgroundPage().console.log("response listener triggered");
+		var rule = {
+			"name": "Access-Control-Allow-Origin",
+			"value": "*"
+		};
 
-	details.responseHeaders.push(rule);
+		details.responseHeaders.push(rule);
 
-	details.responseHeaders.push({"name": "Access-Control-Allow-Methods", "value": "GET, PUT, POST, DELETE, HEAD, OPTIONS"});
+		details.responseHeaders.push({"name": "Access-Control-Allow-Methods", "value": "GET, PUT, POST, DELETE, HEAD, OPTIONS"});
 
-	return {responseHeaders: details.responseHeaders};
-
+		return {responseHeaders: details.responseHeaders};
+	} else {
+		return {};
+	}
 };
 
-function setupListeners() {
+/**
+ * Grabs FA cookies.
+ */
+function refreshFACookies() {
 	chrome.cookies.getAll({
 		url: "https://www.furaffinity.net/"
 	}, function (cookies) {
@@ -38,6 +55,10 @@ function setupListeners() {
 			}
 		}
 	});
+}
+
+function setupListeners() {
+	refreshFACookies();
 	
 	/*Remove Listeners*/
 	chrome.webRequest.onHeadersReceived.removeListener(responseListener);
@@ -55,13 +76,49 @@ function setupListeners() {
 	}, ["blocking", "requestHeaders"]);
 }
 
+/**
+ * Store tab ID on painful.pics sites.
+ */
 chrome.webNavigation.onCompleted.addListener(function (details) {
+	painfulPicsTabIDs.push(details.tabId);
 	setupListeners();
 }, {url: [
 		{hostEquals: 'localhost'},
 		{hostSuffix: 'painful.pics'}
 	]});
 
+/**
+ * Detects FA cookies changes (login in another tab for ex.)
+ */
+chrome.cookies.onChanged.addListener(function(changeInfo) {
+	if (changeInfo.cookie.domain.indexOf('furaffinity.net') > -1) {
+		refreshFACookies();
+	}
+});
+
+/**
+ * Track tab ID opened on painful.pics sites.
+ */
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+	var index = painfulPicsTabIDs.indexOf(tabId);
+	if (index > -1) {
+		painfulPicsTabIDs.splice(index, 1);
+	}
+});
+
+/**
+ * Track tab ID opened on painful.pics sites.
+ */
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	var index = painfulPicsTabIDs.indexOf(tabId);
+	if (index > -1 && (typeof tab.url ==='undefined' || (tab.url.indexOf('localhost:8000') < 0 && tab.url.indexOf('painful.pics') < 0))) {
+		painfulPicsTabIDs.splice(index, 1);
+	}
+});
+
+/**
+ * Used on first-time installation/run.
+ */
 chrome.runtime.onInstalled.addListener(function(){
 	setupListeners();
 	chrome.tabs.query({url: ['https://painful.pics/*', 'http://localhost:8000/*']}, function(tabs) {
